5f990cb259995b1234c08dc20bd3a2fbbb7a6eb9
Sergio Sivelli <srsivelli@hotmail.com>
2022-01-25 12:21:44 +0000

    testes com codesmell

--- src/concat.js
+++ src/concat.js
@@ -5,4 +5,11 @@ function concat(a, b) {
 	return ''.concat(a, b);
 }
 
+function testArray() {
+	let arr = [];
+	arr[0] = 'a';
+	arr['name'] = 'bob';  // Noncompliant
+	arr[1] = 'foo';
+}
+
 module.exports = concat;
--- src/concat.test.js
+++ src/concat.test.js
@@ -3,3 +3,7 @@ const concat = require('./concat');
 test("concat a to b to be equal to ab", () => {
  	expect(concat('a','b')).toBe('ab');
 });
+
+test("concat a to a to be equal to aa", () => {
+	expect(concat('a','a')).toBe('aa');
+});
